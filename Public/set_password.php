8f21cbaba1987f14fc5bc7fb9391d89c3f027cf0
VirgileBernard <bernardvirgile@gmail.com>
2025-03-21 22:48:23 +0100

    UI set_password done

--- Public/set_password.php
+++ Public/set_password.php
@@ -45,9 +45,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Définir votre mot de passe</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/IFAPME/gitIFAHoraire/IFAHoraire/Public/css/calendar.css?v=<?= time(); ?>">
 </head>
-<body>
-    <h1>Définir votre mot de passe</h1>
+<body class="body_set_password">
+    <div class="set_passwordContainer">
+    <h1>Définissez votre mot de passe</h1>
 
     <?php if (isset($error)): ?>
         <p style="color: red;"><?= $error ?></p>
@@ -55,20 +58,23 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <form method="POST">
         <label for="new_password">Nouveau mot de passe :</label>
-        <input type="password" id="new_password" name="new_password" required>
+        <input type="password" id="new_password" name="new_password" required placeholder="mot de passe">
 
         <label for="confirm_password">Confirmer le mot de passe :</label>
-        <input type="password" id="confirm_password" name="confirm_password" required>
+        <input type="password" id="confirm_password" name="confirm_password" required placeholder="confirmation du mot de passe">
 
         <?php if ($role === 'utilisateur'): ?>
+            <div class="select-classe-group">
             <label for="classe">Sélectionnez votre classe :</label>
             <select id="classe" name="classe" required>
                 <option value="1">Classe A</option>
                 <option value="2">Classe B</option>
             </select>
+            </div>
         <?php endif; ?>
 
         <button type="submit">Valider</button>
     </form>
+    </div>
 </body>
 </html>
